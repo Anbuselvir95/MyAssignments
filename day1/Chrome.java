9083bb3ac5601d244ac269e61a485b3d4c8272fd
Anbuselvir95 <anbuselvir95@gmail.com>
2025-06-22 12:11:52 +0530

    Add files via upload
    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float version= 91.0f;
+		System.out.println("Version : "+version);
+
+		String developer="Google";
+		
+		System.out.println("Developer : "+developer);
+
+
+		boolean IsBeta= false;
+		System.out.println("IsBeta : "+IsBeta);
+
+		int releaseYear=2008;		
+		System.out.println("ReleaseYear : "+releaseYear);
+
+		char shortcutkey='C';
+				
+		System.out.println("Shortcutkey : "+shortcutkey);
+		
+		}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float browserversion= 100.2f;
+		System.out.println(browserversion);
+
+		String browserName="firefox";
+		System.out.println(browserName);
+
+		boolean IsVisible= true;
+		System.out.println(IsVisible);
+
+		int releaseYear=1998;
+		System.out.println(releaseYear);
+
+		char browserLogo='f';
+		
+		System.out.println(browserLogo);
+		
+		
+		
+
+	}
+
+}
